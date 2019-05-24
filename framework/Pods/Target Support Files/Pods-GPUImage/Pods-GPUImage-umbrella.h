25d1e23cc4426b65e3e8aedcfb4ec6d975a5a4de
Allen Lin <allenlinli@gmail.com>
2019-05-24 15:53:03 +0800

    pod install to have all the files

---
+++ framework/Pods/Target Support Files/Pods-GPUImage/Pods-GPUImage-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GPUImageVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GPUImageVersionString[];
+
---
+++ framework/Pods/Target Support Files/Pods-GPUImageFramework/Pods-GPUImageFramework-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GPUImageFrameworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GPUImageFrameworkVersionString[];
+
